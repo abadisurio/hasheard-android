5b4d65aed9e8d46498c7bd5f80077e3f557ada52
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-03-24 14:22:38 +0700

    hasheard: implement maps

--- lib/page/root_pic.dart
+++ lib/page/root_pic.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hasheard/widget/maps.dart';
 
 class RootPIC extends StatefulWidget {
   const RootPIC({Key? key}) : super(key: key);
@@ -23,39 +24,53 @@ class _RootPICState extends State<RootPIC> {
     ];
     return Scaffold(
       appBar: getAppBar(),
-      body: getBody(),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          elevation: 5,
-          shadowColor: Colors.black.withOpacity(0.5),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(bottomItems.length, (index) {
-                return IconButton(
-                    onPressed: () {
-                      setState(() {
-                        pageIndex = index;
-                      });
-                    },
-                    icon: Icon(bottomIcons[index]));
-              }),
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          getBody(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              elevation: 5,
+              shadowColor: Colors.black.withOpacity(0.5),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: List.generate(bottomItems.length, (index) {
+                    return IconButton(
+                        onPressed: () {
+                          setState(() {
+                            pageIndex = index;
+                          });
+                        },
+                        icon: Icon(bottomIcons[index]));
+                  }),
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
+      // bottomNavigationBar: Padding(
+      //   padding: const EdgeInsets.all(8.0),
+      //   child:
+      // ),
     );
+  }
+
+  Widget pageOne() {
+    return const MapsWidget();
   }
 
   Widget getBody() {
     return IndexedStack(
       index: pageIndex,
-      children: const [Text("1"), Text("2")],
+      children: [pageOne(), const Text("2")],
     );
   }
 
--- lib/page/root_victim.dart
+++ lib/page/root_victim.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hasheard/widget/maps.dart';
 
 class RootVictim extends StatefulWidget {
   const RootVictim({Key? key}) : super(key: key);
@@ -10,6 +11,10 @@ class RootVictim extends StatefulWidget {
 class _RootVictimState extends State<RootVictim> {
   int pageIndex = 0;
   List<IconData> bottomIcons = [Icons.map_sharp, Icons.person];
+
+  Widget pageOne() {
+    return const MapsWidget();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,39 +28,49 @@ class _RootVictimState extends State<RootVictim> {
     ];
     return Scaffold(
       appBar: getAppBar(),
-      body: getBody(),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          elevation: 5,
-          shadowColor: Colors.black.withOpacity(0.5),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(bottomItems.length, (index) {
-                return IconButton(
-                    onPressed: () {
-                      setState(() {
-                        pageIndex = index;
-                      });
-                    },
-                    icon: Icon(bottomIcons[index]));
-              }),
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          getBody(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              elevation: 5,
+              shadowColor: Colors.black.withOpacity(0.5),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: List.generate(bottomItems.length, (index) {
+                    return IconButton(
+                        onPressed: () {
+                          setState(() {
+                            pageIndex = index;
+                          });
+                        },
+                        icon: Icon(bottomIcons[index]));
+                  }),
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
+      // bottomNavigationBar: Padding(
+      //   padding: const EdgeInsets.all(8.0),
+      //   child:
+      // ),
     );
   }
 
   Widget getBody() {
     return IndexedStack(
       index: pageIndex,
-      children: const [Text("1"), Text("2")],
+      children: [pageOne(), const Text("2")],
     );
   }
 
