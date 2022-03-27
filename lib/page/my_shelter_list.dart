ca3a125483ad5f487f20cfd2b044e9e5997934d5
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-03-27 16:32:11 +0700

    hasheard: implement add shelter button

--- lib/page/my_shelter_list.dart
+++ lib/page/my_shelter_list.dart
@@ -9,15 +9,31 @@ class MyShelterList extends StatefulWidget {
 }
 
 class _MyShelterListState extends State<MyShelterList> {
+  Widget floatingActionButton() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 80.0),
+      child: FloatingActionButton.extended(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        label: Row(children: [Icon(Icons.add), Text("Add New Shelter")]),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.only(
-          left: 16.0, right: 16.0, bottom: 88.0, top: 18.0),
-      itemCount: 10,
-      itemBuilder: (context, index) => const Padding(
-        padding: EdgeInsets.only(bottom: 8.0),
-        child: ShelterItemDetailed(),
+    return Scaffold(
+      floatingActionButton: floatingActionButton(),
+      body: ListView.builder(
+        padding: const EdgeInsets.only(
+            left: 16.0, right: 16.0, bottom: 88.0, top: 18.0),
+        itemCount: 10,
+        itemBuilder: (context, index) => const Padding(
+          padding: EdgeInsets.only(bottom: 8.0),
+          child: ShelterItemDetailed(),
+        ),
       ),
     );
   }
